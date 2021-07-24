9de9745f5da4da9d08ca3e167617c2d94a716d37
Sally Privitera <sallyprivitera@Sallys-MacBook-Air.local>
2021-07-24 22:51:23 +1000

    [minor] Footer styling

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -2,22 +2,34 @@ import Image from 'next/image';
 
 import styles from './styles.module.css';
 import Clickable from '../clickable';
+import { faTimes } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; 
+import { faFacebookSquare, faInstagram } from "@fortawesome/free-brands-svg-icons"
 
 const Footer = () => (
   <footer className={styles.footerWrapper} id="sticky-footer">
     <div className={styles.leftPanel}>
+      <Image width="203" height="61" src="/logo.png" alt="logo" />
       <ul className={styles.contactDetails}>
-        <li>AERODYNAMIC PTY LTD</li>
+        <li><strong>AERODYNAMIC PTY LTD</strong></li>
         <li>+61 3 8331 2900</li>
         <li>31 McGregors Drive</li>
         <li>Keilor Park VIC 3042 </li>
         <li>AUSTRALIA</li>
       </ul>
-      <Image width="203" height="61" src="/logo.png" alt="logo" />
     </div>
     <div className={styles.rightPanel}>
-      <p>Subscribe to Our Newsletter</p>
-      <Clickable type="largeBordered" to="www.google.com">Click Me</Clickable>
+      <div className={styles.subscriptionForm}>
+        <label>Subscribe to Our Newsletter</label>
+        <input type="text" placeholder="Email"/>
+        <Clickable type="largeBordered" to="www.google.com">Subscribe</Clickable>
+      </div>
+      <div className={styles.socials}>
+        <p>Follow us on:</p> <FontAwesomeIcon icon={faFacebookSquare} /> <FontAwesomeIcon icon={faInstagram} />
+      </div>
+    </div>
+    <div className={styles.bottomPanel}>
+      <p>&copy; Copyright AERODYNAMIC PTY LTD</p>
     </div>
   </footer>
 );
--- components/service/index.tsx
+++ components/service/index.tsx
@@ -3,17 +3,23 @@ import Fade from 'react-reveal/Fade';
 
 import styles from './styles.module.css';
 
-const Service = () => {
+interface ServiceProps {
+  title: string,
+  description: string  
+}
+
+const Service = ({title, description}:ServiceProps) => {
   return (
     <Fade>
       <section className={styles.service}>
-      <Fade cascade left>
+  
         <div className={styles.typeWrapper}>
           <div className={styles.iconWrapper}><Image className={styles.icon} width="38" height="46" src="/circular-arrows.svg" alt="logo" /></div>
-          <h1>TITLE TEXT</h1>
+          <h1>{title}</h1>
         </div>
+        <Fade cascade left>
         <div className={styles.body}>
-          <p>Aircraft operators require aircrafts Flight Data and Cockpit Voice Recorders to be analysed annually.  Aerodynamics readout service provides customers with a secure, efficient and personalized service. We work closely with each customer to tailor a program that meets their requirements such as downloading of the Flight data or cockpit voice recorder, maintenance readouts (FDR, CVR and DLR), 24/7 AOG coverage and incident analysis solutions.  Aerodynamic readout service is compliant in accordance with the regulatory authorities mandates for Flight Data and Cockpit Voice recorders.</p>
+          <p>{description}</p>
         </div>
         </Fade>
       </section>
